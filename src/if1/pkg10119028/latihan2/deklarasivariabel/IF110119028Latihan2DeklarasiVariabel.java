882a16b13b9331dd0bb5c13b35f39865f2ff1dc9
Rizal Sihombing <66808677+rizalshmbg@users.noreply.github.com>
2020-10-18 01:03:14 +0700

    Add files via upload

---
+++ src/if1/pkg10119028/latihan2/deklarasivariabel/IF110119028Latihan2DeklarasiVariabel.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119028.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author
+ * NAMA     : Rizal Sihombing
+ * KELAS    : IF1
+ * NIM      : 10119028
+ * Deskripsi Program : Program ini berisi program untuk
+ */
+public class IF110119028Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; // konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+    }
+    
+}
